ebd267a56dc0ea46009d204c04bb34a17be56674
Figueiredo <figo.ppv@gmail.com>
2023-01-08 10:37:32 -0300

    fix migration Files

--- app/Controllers/Http/Users/Main.ts
+++ app/Controllers/Http/Users/Main.ts
@@ -3,7 +3,9 @@ import UpdateValidator from 'App/Validators/User/Main/UpdateValidator'
 
 export default class UsersController {
   public async show({ auth }: HttpContextContract) {
-    const user = auth.user
+    const user = auth.user!
+
+    await user.load('avatar')
 
     return user
   }
